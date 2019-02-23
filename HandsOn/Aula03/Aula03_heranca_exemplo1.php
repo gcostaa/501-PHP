1088221ed13841865b22f548e2c2b9c280ff574d
Gustavo costa <gustavokc05061999@gmail.com>
2019-02-23 15:54:36 -0300

    Aula 03

--- HandsOn/Aula03/Aula03_heranca_exemplo1.php
+++ HandsOn/Aula03/Aula03_heranca_exemplo1.php
@@ -1,5 +1,38 @@
 <?php
 
+class Conta
+{
 
+    public $saldo=0;
+    public $titular;
+
+    /**
+     * Metodo para fazer um saque
+     * @param float $valor->ex de PHP DOC
+     */
+
+     public function sacar($valor)
+     {
+         echo"<hr>Saque Efetuado Valor:<b> $valor</b><hr>";
+         $this->saldo-=$valor;
+     }
+
+     /**
+     * Metodo para fazer um deposito
+     * @param float $valor
+     */
+
+     public function depositar($valor)
+     {
+        echo"<hr>Deposito Efetuado Valor:<b> $valor</b><hr>";
+        $this->saldo+=$valor;
+     }
+
+     public function verSaldo():float
+     {
+         return $this->saldo;
+     }
+
+    }
 
 ?>
