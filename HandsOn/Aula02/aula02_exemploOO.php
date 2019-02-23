02575b485d9debb534149fc5e4c2d3c2af7a606f
Gustavo costa <gustavokc05061999@gmail.com>
2019-02-23 12:56:50 -0300

    Me virando

---
+++ HandsOn/Aula02/aula02_exemploOO.php
@@ -0,0 +1,32 @@
+<?php
+
+/*Alteração para testar git criando uma classe PHP
+para definição do nome usamos o padrão StudlyCaps
+(Sempre iniciando com letra Maiuscula)
+*/
+
+class Conta
+{
+    /*Atributos ou propriedades (visibilidade nome)
+    Para difinição do nome usamos o padrão camelCase*/
+
+    public $saldo;
+    public $titular;
+
+    /*Metodos (visibilidade nome)
+    Metodos podem ou não receber parâmetros, e podem ou não retornar alguma coisa;
+    Para definição do nome usamos o padrão camelCase*/
+
+    public function sacar()
+    {
+
+    }
+
+    public function depositar()
+    {
+
+    }
+
+}
+
+?>
