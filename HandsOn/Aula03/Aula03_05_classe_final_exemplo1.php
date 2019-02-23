056370c393c5a0aef0388c1361c3b45ad44b8de4
Gustavo costa <gustavokc05061999@gmail.com>
2019-02-23 17:44:08 -0300

    aula

---
+++ HandsOn/Aula03/Aula03_05_classe_final_exemplo1.php
@@ -0,0 +1,73 @@
+<?php
+
+abstract class Conta
+//class Conta
+{
+    public $saldo=0;
+    public $titular;
+
+    public function sacar($valor)
+    {
+        $this->saldo-=$valor;
+    }
+
+    public function depositar($valor)
+    {
+        $this->saldo+=$valor;
+    }
+
+    public function verSaldo():float
+    {
+        return $this->saldo;
+    }
+
+    /**Um método abstrato deve ser OBRIGATORIAMENTE
+     * implementado na classe filha
+     *@param string dados
+     */
+
+     public abstract function gerarLog($dados);
+}
+
+final class ContaPoupanca extends Conta
+{
+    public function gerarLog($dados)
+    {
+        echo"<hr>Gerando Log......conta Poupança $dados<br>";
+    }
+}
+
+final class ContaCorrente extends Conta
+{
+    public function gerarLog($dados)
+    {
+        echo"<hr>Gerando Log......conta Corrente $dados<br>";
+    }
+}
+
+class ContaConjunta extends Conta
+{
+    public function gerarLog($dados)
+    {
+        echo"<hr>Gerando Log......conta Corrente $dados<br>";
+    }
+}
+
+class ContaNova extends Conta
+{
+    public function gerarLog($dados)
+    {
+        echo"<hr>Gerando Log......conta Corrente $dados<br>";
+    }
+}
+
+$contaPoupanca = new ContaPoupanca();
+$contaCorrente = new ContaCorrente();
+
+echo '<pre>';
+var_dump($contaCorrente);
+echo'<hr>';
+var_dump($contaPoupanca);
+
+
+?>
