632d9c415cec3a4e0bea3952e486872ec1cdc2c9
Gustavo costa <gustavokc05061999@gmail.com>
2019-03-09 14:09:29 -0300

    Aula04

--- HandsOn/Aula04/Aula04_01_typehinting_exemplo1.php
+++ HandsOn/Aula04/Aula04_01_typehinting_exemplo1.php
@@ -45,7 +45,64 @@ class Titular
 
 class Conta
 {
-    
+    private $agencia;
+
+    private $titular;
+
+    public $saldo;
+
+    public function setAgencia(Agencia $agencia)
+    {
+        $this->agencia=$agencia;
+    }
+
+    public function getAgencia():Agencia
+    {
+       return $this->agencia; 
+    }
+
+    public function setTitular(Titular $titular)
+    {
+        $this->titular = $titular;
+    }
+
+    public function getTitular():Titular
+    {
+        return $this->titular;
+    }
 }
+
+$objAgencia = new Agencia();
+$objAgencia->nome = 'VL Mariana - VLM';
+$objAgencia->telefone = '4044-2515';
+
+$objTitular = new Titular();
+$objTitular->setDados([
+    
+    'nome'=>'Paula',
+    'celular'=>'123456'
+]);
+
+$objConta = new Conta();
+
+$objConta->setAgencia($objAgencia);
+$objConta->setTitular($objTitular);
+$objConta->saldo=500;
+
+echo '<pre>';
+var_dump($objAgencia);
+echo'<hr>';
+var_dump($objTitular);
+echo'<hr>';
+var_dump($objConta);
+echo'<hr>';
+var_dump($objConta->getAgencia());
+echo'<hr>';
+echo'<hr>';
+
+echo 'Agencia: '.$objConta->getAgencia()->getNome(). ' - '.$objConta->getAgencia()->getTelefone().'<br>'; 
+echo 'Titular: '.$objConta->getTitular()->getNome(). ' - '.$objConta->getTitular()->getCelular().'<br>'; 
+
+echo 'Saldo: '.$objConta->saldo;
 
 ?>
