255b1c68d464e1717aba998ff88bbf1b255e506a
Gustavo costa <gustavokc05061999@gmail.com>
2019-03-09 13:10:42 -0300

    Aula04

---
+++ HandsOn/Aula04/Aula04_01_typehinting_exemplo1.php
@@ -0,0 +1,25 @@
+<?php
+
+class Agencia
+{
+    public $nome;
+
+    public $telefone;
+
+    public function getNome(){
+
+        return $this->nome;
+    }
+
+    public function getTelefone(){
+        
+        return $this->telefone;
+    }
+}
+
+class Titular
+{
+    
+}
+
+?>
