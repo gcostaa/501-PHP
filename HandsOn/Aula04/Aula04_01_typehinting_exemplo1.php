f94c25bd7aa8fc6cd233799674ddbb308253fae2
Gustavo costa <gustavokc05061999@gmail.com>
2019-03-09 13:15:52 -0300

    Aula04

--- HandsOn/Aula04/Aula04_01_typehinting_exemplo1.php
+++ HandsOn/Aula04/Aula04_01_typehinting_exemplo1.php
@@ -19,7 +19,28 @@ class Agencia
 
 class Titular
 {
+
+    private $nome;
     
+    private $celular;
+
+    public function setDados(array $dados)
+    {
+        $this->nome=$dados['nome'];
+        $this->celular=$dados['celular'];
+
+    }
+
+    public function getNome(){
+        
+        return $this->nome;
+    }
+
+
+    public function getCelular(){
+        
+        return $this->celular;
+    }
 }
 
 ?>
