981a16c267ac8dfa0896d43c3bc5edd80c951d7f
Gustavo costa <gustavokc05061999@gmail.com>
2019-03-16 17:32:56 -0300

    construct_destruct

--- HandsOn/Aula08/Aula08_01_metodos_magicos_cons_dest.php
+++ HandsOn/Aula08/Aula08_01_metodos_magicos_cons_dest.php
@@ -24,6 +24,43 @@
         {
             $this->saldo;
         }
+
+        public function getTitular()
+        {
+           return $this->titular;
+        }
+
+        public function __destruct()
+        {
+            echo '<hr>Metodo <b>' .__METHOD__ . '</b> executando na classe: <b>' .__CLASS__ .'</b><hr>';
+        }
     }
+
+    Class Titular
+    {
+        public $nome;
+        public $cpf;
+
+        public function __destruct()
+        {
+            echo '<hr>Metodo <b>' .__METHOD__ . '</b> executando na classe: <b>' .__CLASS__ .'</b><hr>';
+        }
+    }
+
+    $titular = new Titular();
+    $titular->nome='Paula';
+    $titular->cpf='123456';
+
+    $conta = new Contas($titular);
+
+    unset($titular);
+
+    $titular = new Titular();
+    $titular->nome='Paula';
+    $titular->cpf='123456';
+
+    echo 'Titular: '.$conta->getTitular()->nome;
+    echo '<br>CPF Titular: '.$conta->getTitular()->cpf;
+
 
 ?>
